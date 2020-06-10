2121a131e43b7c2d19226aa9f4bdc1de1a7d3832
Joyce Lin <joycelin32123l@gmail.com>
2020-06-10 12:02:41 +1000

    commit

--- week2/exercise1.py
+++ week2/exercise1.py
@@ -13,16 +13,20 @@ print("hello! Let's get started")  # it printed "hello! Let's get started"
 
 some_words = ['what', 'does', 'this', 'line', 'do', '?']
 
+   # I think this will print "what" imported from some_words calling the print function
 for word in some_words:
-    print(word)
+    print(word) # It printed the words in some_words individually in separate lines
 
+    # I think this will print "?" imported from some_words that isn't a word calling the print function 
 for x in some_words:
-    print(x)
+    print(x) # It also prints the words in some_words individually in separate lines
 
-print(some_words)
+# I think this will print "what does this line do ?"
+print(some_words) # It printed some_words exactly by the layout it was in
 
+#I think this will print "what does this line do ? contains more than 3 words"
 if len(some_words) > 3:
-    print('some_words contains more than 3 words')
+    print('some_words contains more than 3 words') # It printed some_words contains more than 3 words
 
 def usefulFunction():
     """
@@ -30,6 +34,8 @@ def usefulFunction():
     what the line below does:
     https://docs.python.org/3/library/platform.html#platform.uname
     """
-    print(platform.uname())
+    # I think this will disregard items that are undefined, they become 'none'
+    print(platform.uname()) # Instead of not naming things, it named all the platforms used
 
-usefulFunction()
+# I think this addresses the information as important
+usefulFunction() # this wasn't a code?
